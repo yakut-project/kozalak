08266e43f4debf6d59e055c7ab516467dbca5a5e
Halit Alptekin <info@halitalptekin.com>
2013-08-28 10:22:17 +0300

    #1 create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kozalak::Application.config.secret_key_base = '99d5eb9a2ab73191b5b32c7b0b88e159199e56fafc558e7c35d36451b9ca0dda6933c524bf0eeee8e39c73f409a4a60f68872595f34e3fe1fa6d5f33f7e63515'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kozalak::Application.config.session_store :cookie_store, key: '_kozalak_session'
