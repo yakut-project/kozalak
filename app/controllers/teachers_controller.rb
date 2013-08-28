e6f55af2a72523e080af71cd860ee53848f7dc6d
Bilgehan Varli <bilgehanvarli@gmail.com>
2013-08-28 11:59:45 +0300

    #4 - Authorization privileges fixed

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,6 +1,6 @@
 class TeachersController < ApplicationController
   before_action :set_teacher, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_teacher!, only: [:show,:index]
   # GET /teachers
   # GET /teachers.json
   def index
