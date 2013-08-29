e99dfacd9008d9b078d34ce3f40fa3940ce8fbc2
Halit Alptekin <info@halitalptekin.com>
2013-08-29 13:10:23 +0300

    #9 add database seed for new models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Admin.create( :email => 'admin1@kozalak.com', :password => '12345678', :password
 end
 
 30.times do |i|
-  User.create( :email => "user#{i}@kozalak.com", :password => '12345678', :password_confirmation => '12345678')
+  User.create( first_name => "user#{i}", last_name => "userlastname#{i}", :website => "http://username#{i}.com", :github_account => "user#{i}", :bio => "useruser #{i}", :email => "user#{i}@kozalak.com", :password => '12345678', :password_confirmation => '12345678')
 end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
